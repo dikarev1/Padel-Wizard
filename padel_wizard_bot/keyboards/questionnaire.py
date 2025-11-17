cafe5c160cc8604a7ebd67da9fccc58237a06176
pasha <dikarev.ff@gmail.com>
2025-11-18 02:50:53 +0400

    adjust buttons

--- padel_wizard_bot/keyboards/questionnaire.py
+++ padel_wizard_bot/keyboards/questionnaire.py
@@ -14,7 +14,7 @@ def build_question_keyboard(question: Question) -> ReplyKeyboardBuilder:
     for option in question.options:
         builder.button(text=option.text)
 
-    if question.id in {"q2", "q1.1"}:
+    if question.id in { "q1", "q1.1", "q2", "q3", "q4", "q5"}:
         builder.adjust(2)
     else:
         builder.adjust(1)
